54ba95e366c1af7c82a19df1c6afd5bdc1a10901
Bixler Baker <bibaker@adit.mines.edu>
2016-10-04 14:58:37 -0600

    Skeleton for borad and door direction

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -1,5 +1,35 @@
 package clueGame;
 
-public class Board {
+import java.util.Map;
 
+public class Board {
+	private static Board theInstance = new Board();
+	
+	private Board() {}
+	
+	public static Board getInstance() { return theInstance; }
+	
+	public void setConfigFiles(String map, String legend) {
+		
+	}
+	
+	public void initialize() {
+		
+	}
+	
+	public Map<Character, String> getLegend() {
+		return null;
+	}
+	
+	public int getNumRows() {
+		return 0;
+	}
+	
+	public int getNumColumns() {
+		return 0;
+	}
+	
+	public BoardCell getCellAt(int row, int column) {
+		return null;
+	}
 }
